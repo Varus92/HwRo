aaf6faf6937d4cc49b123de8e1fb01b974caaaab
Varus92 <48761963+Varus92@users.noreply.github.com>
2020-05-21 16:48:46 +0200

    fixed error grasp

--- Grasp.c
+++ Grasp.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 
 
-bool time_limit(instance* inst);
 void Grasp(instance* inst, int* succ, int* comp, int* ncomp);
 
 void heuristicSolver(instance* inst)
@@ -23,7 +22,7 @@ void heuristicSolver(instance* inst)
 void Grasp(instance* inst, int* succ, int* comp, int* ncomp)
 {
 	//Componenti iniziali
-	double bestsol = 999999;
+	double best_sol = 9999999;
 	*ncomp = 1;
 	int* tmp_succ = (int*)calloc(inst->nnodes, sizeof(int));
 	
@@ -33,93 +32,90 @@ void Grasp(instance* inst, int* succ, int* comp, int* ncomp)
 		comp[i] = 1;
 	}
 
-	while (!time_limit(inst))
+	double t_span = 0; 
+
+	while (t_span < inst->timelimit)
 	{
-		if (VERBOSE > 100) printf("Trying a new starting node...");
+		//if (VERBOSE > 100) printf("Trying a new starting node...");
 
 		//Choose starting point
 		srand(time(NULL));
-		int p = rand() % inst->nnodes;
-		int start = p;
+		int n = rand() % inst->nnodes;
+		int start = n;
 		bool done = false;
-		int solcost = 0;
+		int sol_cost = 0;
 
 		while (!done)
 		{
 			//Il prossimo nodo da visitare sarà scelto tra 3 nodi possibili
-			int min_dist1 = 9999999;
-			int min_dist2 = 9999999;
-			int min_dist3 = 9999999;
-			int next_node[3] = { -1 };
+			int min_dist1 = 99999999;
+			int min_dist2 = 99999999;
+			int min_dist3 = 99999999;
+			int next_node[3] = {-1,-1,-1};
 			int final_node = -1;
 
 			for (int i = 0; i < inst->nnodes; i++)
 			{
-				if (i != p && min_dist1 > dist(p, i, inst) && tmp_succ[i] == -1)
+				if (i != n && min_dist1 > dist(i, n, inst) && tmp_succ[i] == -1)
 				{
 					min_dist3 = min_dist2;
 					min_dist2 = min_dist1;
-					min_dist1 = dist(p, i, inst);
+					min_dist1 = dist(i, n, inst);
 					next_node[0] = i;
 				}
-				else if (i != p && min_dist2 > dist(p, i, inst) && tmp_succ[i] == -1)
+				else if (i != n && min_dist2 > dist(i, n, inst) && tmp_succ[i] == -1)
 				{
 					min_dist3 = min_dist2;
-					min_dist2 = dist(p, i, inst);
+					min_dist2 = dist(n, i, inst);
 					next_node[1] = i;
 				}
-				else if (i != p && min_dist3 > dist(p, i, inst) && tmp_succ[i] == -1)
+				else if (i != n && min_dist3 > dist(i, n, inst) && tmp_succ[i] == -1)
 				{
-					min_dist3 = dist(p, i, inst);
+					min_dist3 = dist(i, n, inst);
 					next_node[2] = i;
 				}
 			}
 
 			//Sceglamo in modo random Tra i 3 valori
-			//srand(time(NULL));
-			int n = rand() % 3;
-			final_node = next_node[n];
-
+			if (next_node[0] != -1 || next_node[1] != -1 || next_node[2] != -1)
+			{
+				while (final_node == -1)
+				{
+					srand(time(NULL));
+					int r = rand() % 3;
+					final_node = next_node[r];
+				}
+			}
+			
 			//Verifico se il circuito è chiuso
 			if (final_node == -1)
 			{
-				tmp_succ[p] = start;
-				solcost += dist(p, start, inst);
+				tmp_succ[n] = start;
+				sol_cost += dist(n, start, inst);
 				done = true;
 			}
 			else //Aggiorno i valori
 			{
-				solcost += dist(p, final_node, inst);
-				tmp_succ[p] = final_node;
-				p = final_node;
+				sol_cost += dist(n, final_node, inst);
+				tmp_succ[n] = final_node;
+				n = final_node;
 			}
 		}
 
 		//Aggiorno la migliore soluzione
-		if (solcost < bestsol)
+		if (sol_cost < best_sol)
 		{
 			for (int i = 0; i < inst->nnodes; i++)
 				succ[i] = tmp_succ[i];
-			bestsol = solcost;
+			best_sol = sol_cost;
 		}
 
-		//Reset tmp_succ
-		for (int i = 0; i < inst->nnodes; i++)
+		/*for (int i = 0; i < inst->nnodes; i++)
 			tmp_succ[i] = -1;
-
-		inst->best_lb = solcost;
+		*/
+		inst->best_lb = sol_cost;
 	}
 	free(tmp_succ);
 }
 
 
-bool time_limit(instance* inst)
-{
-	double tspan = second() - inst->tstart;
-	if (tspan > inst->timelimit)
-	{
-		if (VERBOSE >= 100) printf("\n\n$$$ time limit of %10.1lf sec.s expired after %10.1lf sec.s $$$\n\n", inst->timelimit, tspan);
-		return true;
-	}
-	return false;
-}
